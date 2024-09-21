671ae224afa0e63b7db9407bf5c4e90660a4e108
bryaanls <bryanluccas@hotmail.com>
2024-09-21 14:01:09 -0300

    feat: populate function

---
+++ src/services/populate.js
@@ -0,0 +1,32 @@
+import cliProgress from 'cli-progress';
+import { generateSimpleDocument, generateComplexDocument } from './generate-documents.js';
+import { insertDocuments } from '../services/insert-documents.js';
+
+export const populateDB = async (options) => {
+  let simpleCollectionsPromises,
+    complexCollectionsPromises = [];
+
+  const multiBar = new cliProgress.MultiBar({
+    format: '{collectionName} | {bar} | {percentage}% | {duration_formatted} | {value}/{total}',
+    barCompleteChar: '※',
+    barIncompleteChar: '⁍',
+  });
+
+  const { simpleCollections, complexCollections } = options;
+
+  if (simpleCollections) {
+    simpleCollectionsPromises = simpleCollections.map(async (collectionName) => {
+      await insertDocuments(collectionName, generateSimpleDocument, options, multiBar);
+    });
+  }
+
+  if (complexCollections) {
+    complexCollectionsPromises = complexCollections.map(async (collectionName) => {
+      await insertDocuments(collectionName, generateComplexDocument, options, multiBar);
+    });
+  }
+
+  await Promise.all([...simpleCollectionsPromises, ...complexCollectionsPromises]);
+  multiBar.stop();
+  console.log('Banco de dados populado com sucesso!');
+};
