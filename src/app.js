2de549273a15a82b635fe0764e401b65be3fe50c
BryaanLs <bryanluccas@hotmail.com>
2024-09-30 09:35:51 -0300

    feat: add db name on enviroment variables in docker-compose file

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-import { conn } from './db/conn.js';
-import { populateDB } from './services/populate.js';
+import { conn } from "./db/conn.js";
+import { populateDB } from "./services/populate.js";
 
 const simpleCollections = [
-  'simpleCollection1',
-  'simpleCollection2',
-  'simpleCollection3',
-  'simpleCollection4',
+  "simpleCollection1",
+  "simpleCollection2",
+  "simpleCollection3",
+  "simpleCollection4",
 ];
-const complexCollections = ['complexCollection1', 'complexCollection2'];
+const complexCollections = ["complexCollection1", "complexCollection2"];
 
 const main = async () => {
   const client = await conn();
-  const db = client.db('myDB');
+  const db = client.db(process.env.DB_NAME ?? "myDatabase");
 
   const options = {
     simpleCollections: simpleCollections,
     complexCollections: complexCollections,
-    collectionSize: 1e6,
+    collectionSize: 1e5,
     batchSize: 2500,
     db,
-    concurrence: 2
+    concurrence: 2,
   };
 
   await populateDB(options);
